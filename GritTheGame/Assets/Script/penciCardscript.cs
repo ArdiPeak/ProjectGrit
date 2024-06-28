1a21f8f187ae5f6f297cc772981e586b29eb3beb
ArdiPeak <afix05element@gmail.com>
2024-06-28 20:01:25 +0800

    Select card

---
+++ GritTheGame/Assets/Script/penciCardscript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class penciCardscript : MonoBehaviour
+{
+    public int level = 0; //rarity C
+    public int dmg = 3;
+    public int cost = 1;
+    public string cardName = "Pencils Throw";
+    public string cardDesc = "Yeet the pencils";
+    public char cardCode = 'P';
+
+    void OnMouseDown(){
+       Debug.Log("Selected Pencil");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
